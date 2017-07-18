e789e62e5c57342e37ff101a021faf551dd53a20
magentaFire <dafne.magenta@gmail.com>
2017-07-18 16:14:47 -0500

    Falta arreglar los errores que genra el cambio de tamaño de la pantalla en el index

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,12 @@
-$('.carousel.carousel-slider').carousel({fullWidth: true});
-$('.carousel.carousel-slider').css({
-    "height": "65vh"
-});
+$(document).ready(start);
+
+function start(){
+    $('.carousel.carousel-slider').carousel({fullWidth: true});
+    $('.carousel.carousel-slider').css({
+        "height": "46vh"
+    });
+
+    $("#btnRegistro").click(function(){
+        location.href = "public/registro.html";
+    });
+}
