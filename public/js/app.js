51dbf5fb9526722c8c267ebb519bc3100ac52b35
magentaFire <dafne.magenta@gmail.com>
2017-07-19 14:51:20 -0500

    Terminado reto 1

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,31 @@
 $(document).ready(start);
 
-function start(){
-    $('.carousel.carousel-slider').carousel({fullWidth: true});
+function start() {
+    $('.carousel.carousel-slider').carousel({ fullWidth: true });
     $('.carousel.carousel-slider').css({
         "height": "46vh"
     });
 
-    $("#btnRegistro").click(function(){
+    $("#btnRegistro").click(function () {
         location.href = "public/registro.html";
     });
+
+    // Scripts para pagina 1 de verificacion de datos
+    $("#phone").keyup(verificaDatos);
+    $("#terms").click(verificaDatos);
+    $("#btnContinuar").click(enviaJSON);
+
+    function verificaDatos() {
+        if ($("#phone").val().length == 10 && $("#terms").is(":checked")) {
+            $("#btnContinuar").removeClass("disabled");
+        }
+        else{
+            $("#btnContinuar").addClass("disabled")
+        }
+    }
+
+    function enviaJSON(){
+        var url = "http://localhost:3000/api/registerNumber"
+        $.post(url, {})
+    }
 }
